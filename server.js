2aa722edd0e671e3c353c22203c93fef41375948
Cassio Caue da Cunha <cassio.cunha@sensedia.local>
2020-08-29 18:40:47 -0300

    one product sucess

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const express = require('express');
+
 const MongoClient = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
+const Product = mongoose.model('Product');
+
+// Agrupando Models
+const requireDir = require('require-dir');
+requireDir("./src/models");
+
 // Iniciando o Server
 const app = express();
-
 // Iniciando o BD
-MongoClient.connect("mongodb://localhost:27017/nodeapi", {useUnifiedTopology: true})
+mongoose.connect("mongodb://localhost:27017/nodeapi");
 
 //Primeira rota
 app.get('/', (req, res) => {
-    res.send("Tudo ok!")
+        Product.create({
+            title: "React Naterwerive",
+            description: "Build Nwerwerative apps witch React",
+            url: "http://github.com/fawerwercebook/react-native"
+        });
+        return res.send("Tudo ok!");
 })
 
 app.listen(3001);
