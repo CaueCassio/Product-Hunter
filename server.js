75bb29d97a7853b8b46307cbdcb49c7a348e50c9
Cassio Caue da Cunha <cassio.cunha@sensedia.local>
2020-08-29 00:13:58 -0300

    conect mongodb

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require ('express');
+const mongoose = require('mongoose');
+
+// Iniciando o Server
 const app = express();
 
+// Iniciando o BD
+mongoose.connect('mongodb://localhost:27017/')
+
+//Primeira rota
 app.get('/', (req,res)=>{
     res.send("Tudo ok!")
 })
-
 
 app.listen(3001);
