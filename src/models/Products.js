c89808eaf757b44880ddff92b1703344c9ee516c
Cassio Caue da Cunha <cassio.cunha@sensedia.local>
2020-08-31 17:34:44 -0300

    implements moongose-paginates

--- src/models/Products.js
+++ src/models/Products.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate');
 
 const ProductSchema = new mongoose.Schema({
 
@@ -20,5 +21,7 @@ CreatedAt:{
     default: Date.now,
 },
 });
+
+ProductSchema.plugin(mongoosePaginate);
 
 mongoose.model('Product', ProductSchema);
